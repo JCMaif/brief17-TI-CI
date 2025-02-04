24f4805308ae4ae718a03247485ddf42e3571930
Jean-Charles Audebert <jean-charles.audebert@maif.fr>
2025-02-04 10:12:03 +0100

    test integration saveStudent pass

--- src/test/java/com/example/brief17/StudentServiceIntegrationTest.java
+++ src/test/java/com/example/brief17/StudentServiceIntegrationTest.java
@@ -1,23 +1,46 @@
 package com.example.brief17;
 
+import com.example.brief17.repository.StudentRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.example.brief17.entity.Student;
-import com.example.brief17.service.StudentService;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Optional;
 
-// TODO: Ajouter les tags nécessaires pour charger H2, charger le profil de test et importer le StudentService
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@DataJpaTest
+@ActiveProfiles("test")
 class StudentServiceIntegrationTest {
 
+    @Autowired
+    private StudentRepository studentRepository;
+
     @Test
+    @DisplayName("Nominal Case test : save Student and return it")
     void shouldSaveAndRetrieveStudent() {
-        // TODO: Implémenter le test d'intégration, insérer un Student en base de données et le récupérrer
+        //Arrange
+        final Student student = new Student();
+        student.setName("Sarah Connor");
+        student.setAddress("La Fistinière");
+
+        //Act
+        Student savedStudent = studentRepository.save(student);
+        Optional<Student> foundStudent = studentRepository.findById(savedStudent.getId());
+
+        //Assert
+        assertThat(foundStudent)
+                .isPresent()
+                .hasValueSatisfying( (s) -> {
+                    assertThat(s.getName()).isEqualTo(savedStudent.getName());
+                    assertThat(s.getAddress()).isEqualTo(savedStudent.getAddress());
+                })
+        ;
+
     }
 }
